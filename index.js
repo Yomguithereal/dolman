d83be380d5b8d26ef97472217fdd0db483b522e0
Yomguithereal <guillaumeplique@gmail.com>
2015-12-18 16:45:11 +0100

    Yup

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
  * Exposing the library's utilities.
  */
 var responses = require('./src/responses.js'),
-    Typology = require('typology');
+    middlewares = require('./src/middlewares.js'),
+    Typology = require('typology'),
+    util = require('util');
 
 module.exports = function(express, opts) {
   opts = opts || {};
@@ -20,4 +22,48 @@ module.exports = function(express, opts) {
 
   // Applying responses to express
   responses(express);
+
+  // Building the router function
+  function makeRouter(routes, o) {
+    o = o || {};
+
+    var router = express.Router(),
+        before = [].concat(o.before || []),
+        after = [].concat(o.after || []);
+
+    routes.forEach(function(route) {
+      if (!route.url)
+        throw Error('dolman.router: one route has no url: ' + util.inspect(route));
+
+      if (!route.action)
+        throw Error('dolman.router: the route for url ' + route.url + ' has no action.');
+
+      // Applying before middlewares
+      var routeMiddlewares = before;
+
+      // Validation
+      if (route.validate)
+        routeMiddlewares.push(middlewares.validate(types, route.validate));
+
+      // Applying after middlewares
+      routeMiddlewares = routeMiddlewares.concat(after);
+
+      // Determining the method
+      var methods = [].concat(route.method || route.methods || 'GET');
+
+      methods.forEach(function(method) {
+        router[method.toLowerCase()].apply(
+          router,
+          [route.url].concat(routeMiddlewares).concat(route.action)
+        );
+      });
+    });
+
+    return router;
+  };
+
+  // Returning an object to handle
+  return {
+    router: makeRouter
+  };
 };
